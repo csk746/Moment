f929ecd407c7cc64a088a13e4e4e53a72c406a86
geonhee lee <csk746@gmail.com>
2017-11-18 01:57:24 +0900

    메인화면 스타일 추가

--- app/container/Main.js
+++ app/container/Main.js
@@ -4,13 +4,20 @@ import { Actions } from 'react-native-router-flux';
 import Recommand from '../component/recommend'
 import DirectorySwipe from '../component/directorySwipe'
 import PictureBrowser from '../component/picureBrowser'
+
 export default class Main extends Component {
   render() {
     return (
-      <View>
-        <Recommand/>
-        <DirectorySwipe/>
-        <PictureBrowser/>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, backgroundColor: 'gray' }}>
+          <Recommand />
+        </View>
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+          <DirectorySwipe />
+        </View>
+        <View style={{ flex: 4, backgroundColor: 'gray' }}>
+          <PictureBrowser />
+        </View>
       </View>
     )
   }
