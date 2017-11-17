6f9d316527ce5fa018d19144c0c80894aafa725b
geonhee lee <csk746@gmail.com>
2017-11-18 01:18:46 +0900

    첫 커밋. react-native-router-flux 구축

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Router, Scene } from 'react-native-router-flux';
+import Main from './app/Main';
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Scene key="root">
+        <Scene key="main" component={Main} initial={true} />
+        </Scene>
+      </Router>
+    );
+  }
+}
