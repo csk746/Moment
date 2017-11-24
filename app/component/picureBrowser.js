ca2f82ea2d91a3df0f9d60cf3d6484f0c7bbe3f6
eunbeejo <joeunbi0724@gmail.com>
2017-11-24 17:12:04 +0900

    scrollView, Modal

--- app/component/picureBrowser.js
+++ app/component/picureBrowser.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+    ScrollView,
     StyleSheet,
     Text,
     View,
@@ -38,33 +39,36 @@ export default class PictureBrowser extends Component {
     }
 
     render() {
+
         let images = this.state.images.map((val, key) => {
             return <TouchableWithoutFeedback key={key}
                                              onPress={() => { this.setModalVisible(true, key)}}>
-                        <View style={styles.imagewrap}>
-                            <ImageElement imgsource={val}></ImageElement>
-                        </View>
-                   </TouchableWithoutFeedback>
+                <View style={styles.imagewrap}>
+                    <ImageElement imgsource={val}></ImageElement>
+                </View>
+            </TouchableWithoutFeedback>
         });
 
         return(
-            <View style={styles.container}>
+            <ScrollView>
+                <View style={styles.container}>
+                    <Modal style={styles.modal} animationType={'fade'}
+                           transparent={true} visible={this.state.modalVisible}
+                           onRequestCose={() => {}}>
 
-                /* Modal 넣으면 error */
-                {/*<Modal style={styles.modal} animationType={'fade'}*/}
-                       {/*transparent={true} visible={this.state.modalVisible}*/}
-                       {/*onRequestClose{() => {}}>*/}
+                        <View style={styles.modal}>
+                            <Text style={styles.text}
+                                  onPress={() => {this.setModalVisible(false)}}>Close</Text>
+                            <ImageElement imgsource={this.state.modalImage}></ImageElement>
+                        </View>
 
-                    {/*<View style={styles.modal}>*/}
-                        {/*<Text style={styles.text}*/}
-                              {/*onPress={() => {this.setModalVisible(false)}}>Close</Text>*/}
-                        {/*<ImageElement imgsource={this.state.modalImage}></ImageElement>*/}
-                    {/*</View>*/}
+                    </Modal>
 
-                {/*</Modal>*/}
-                { images }
-            </View>
+                    { images }
+                </View>
+            </ScrollView>
         );
+
     }
 }
 
