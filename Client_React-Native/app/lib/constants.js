4d836d9154db3afd7a530eed8f7d74a21390f258
Lama <lama@Lamaui-MacBook-Pro.local>
2017-12-15 12:56:43 +0900

    React Native 제거

--- Client_React-Native/app/lib/constants.js
+++
@@ -1,5 +0,0 @@
-import keyMirror from 'key-mirror'
-
-export default keyMirror({
-    ADD_IMAGE:null
-})
--- Client_React-Native/ios/Pods/Target Support Files/Pods-momentTests/Pods-momentTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_momentTests : NSObject
-@end
-@implementation PodsDummy_Pods_momentTests
-@end
